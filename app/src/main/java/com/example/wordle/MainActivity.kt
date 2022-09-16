8d3f0d5eafdf0f487ae56a9d56c8b283bbbb99c2
Luigi Otoya <Luigi.Otoya@gmail.com>
2022-09-16 14:34:09 -0400

    fixed repeated highlights and added more words

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.SpannableStringBuilder
 import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.core.text.color
@@ -14,7 +16,6 @@ import nl.dionsegijn.konfetti.core.Party
 import nl.dionsegijn.konfetti.core.Position
 import nl.dionsegijn.konfetti.core.emitter.Emitter
 import nl.dionsegijn.konfetti.xml.KonfettiView
-import org.w3c.dom.Text
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
@@ -35,7 +36,7 @@ class MainActivity : AppCompatActivity() {
             .append("letter in correct spot\n")
             .color(yellow, { append("Yellow: ")})
             .append("letter in wrong spot")
-        val tipsTextView = findViewById<TextView>(R.id.tips).setText(tipsText)
+        findViewById<TextView>(R.id.tips).setText(tipsText)
 
         val userInput = findViewById<EditText>(R.id.userInput)
         val submitBtn = findViewById<Button>(R.id.submitBtn)
@@ -58,14 +59,17 @@ class MainActivity : AppCompatActivity() {
         })
         // Submit Button
         submitBtn.setOnClickListener {
+            closeKeyboard()
             val word = userInput.text.toString().uppercase()
             println(word)
             if (checkValid(word)) {
                 insertWord(word, tvIDs)
 
+                // On Correct Guess
                 if (checkGuess(word)) {
                     userInput.isEnabled = false
                     Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+
                     val party = Party(
                         speed = 0f,
                         maxSpeed = 30f, damping = 0.9f,
@@ -78,15 +82,16 @@ class MainActivity : AppCompatActivity() {
                     konfettiView.start(party)
                 }
                 tries++
+                if (tries == 3) {
+                    userInput.isEnabled = false
+                    tryAgainMsg.isVisible = true
+                }
             }
             else {
                 Toast.makeText(it.context, "Not in Word List", Toast.LENGTH_SHORT).show()
             }
             userInput.text = null
-            if (tries == 3) {
-                userInput.isEnabled = false
-                tryAgainMsg.isVisible = true
-            }
+
         }
 
         // Reset Button
@@ -112,18 +117,39 @@ class MainActivity : AppCompatActivity() {
     }
     // insert guess into table
     private fun insertWord(word: String, ids: Array<Array<Int>>) {
+        // frequency map for letters in the word to guess
+        val countMap = HashMap<Char, Int>()
+        for (ch in wordToGuess) {
+            if (countMap.containsKey(ch))
+                countMap[ch] = countMap.getValue(ch)+1
+            else
+                countMap[ch] = 1
+        }
+        //println(countMap)
         for (i in 0..3) {
             val tv = findViewById<TextView>(ids[tries][i])
             tv.text = word[i].toString()
             if (word[i] == wordToGuess[i]) {
                 tv.setTextColor(ContextCompat.getColor(this, R.color.Teal_A400))
-            } else if (word[i] in wordToGuess) {
+                countMap[word[i]] = countMap.getValue(word[i])-1
+            } else if ( (word[i] in wordToGuess) && (countMap.getValue(word[i]) > 0) ) {
                 tv.setTextColor(ContextCompat.getColor(this, R.color.Lime_A200))
+                countMap[word[i]] = countMap.getValue(word[i])-1
+                //println(countMap)
             }
         }
     }
     // check if guess matches the word
     private fun checkGuess(guess: String) : Boolean {
         return guess == wordToGuess
+    }
+    // Close Keyboard
+    private fun closeKeyboard() {
+        val view: View? = this.currentFocus
+        if (view != null) {
+            val inputMethodManager =
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
